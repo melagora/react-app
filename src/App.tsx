1043a1bfa884823547a6813cc07378f5e04f3a21
Melvin González <54681730+melagora@users.noreply.github.com>
2025-02-17 11:10:20 -0600

    Entendiendo el uso de fragments <>...</>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Card from "./components/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return <Card></Card>;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,17 +2,24 @@ function Card() {
   return (
     <div className="card" style={{ width: "350px" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <CardBody />
       </div>
     </div>
   );
 }
 
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
+  );
+}
 export default Card;
