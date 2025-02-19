e8654b66fa3bc3f8f5a6682ee56876e2bfb0b519
Melvin González <54681730+melagora@users.noreply.github.com>
2025-02-18 19:53:02 -0600

    Desde un componente padre pasamos una lista e iteramos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const list = ["Nombre1", "Nombre2", "Nombre3", "Nombre4"];
+
   return (
     <Card>
       <CardBody title="Hola mundo" text="Este es el texto" />
+      <List data={list} />
     </Card>
   );
 }
