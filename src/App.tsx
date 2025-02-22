86e8fa9909f989dc931c1aabfe6604a3ee1983c8
Melvin González <54681730+melagora@users.noreply.github.com>
2025-02-21 21:15:17 -0600

    Funciones como Props (Acciones al seleccionar un item de la lista) *Reutilizable*

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const list = ["Nombre1", "Nombre2", "Nombre3", "Nombre4"];
 
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+
+  const handleSelect2 = (elemento: string) => {
+    console.log("Mostrando", elemento);
+  };
+
   return (
     <Card>
       <CardBody title="Hola mundo" text="Este es el texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
